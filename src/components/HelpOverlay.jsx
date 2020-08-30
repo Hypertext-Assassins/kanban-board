849b842f13e7c899e5b23e89604aab6ff2ceca8c
Alanna Celentano <celentanoad91@gmail.com>
2020-08-30 17:33:01 -0400

    add ? no functionality yet

---
+++ src/components/HelpOverlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Overlay = () => {
+    return ( 
+        <div>
+        <i className="question circle outline icon"></i>
+        </div>
+     );
+}
+ 
+export default Overlay;
